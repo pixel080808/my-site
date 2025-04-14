8be87d09d2d8e0ed7c1ebd27ff51e2c6d44ec8af
Vitalii <pixel080808@gmail.com>
2025-04-15 01:09:06 +0300

    Оновлення

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,13 @@
 // models/Category.js
+const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     slug: { type: String, unique: true, required: true },
     image: String,
-    parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', default: null },
     subcategories: [{
         name: { type: String, required: true },
         slug: { type: String, required: true },
-        image: { type: String, default: '' }
+        image: { type: String, default: '' } // Додаємо поле image
     }]
 }, { timestamps: true });
 module.exports = mongoose.model('Category', categorySchema);
