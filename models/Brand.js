1c827162cafa5c1496b8b61437f5b8172b5a6806
Vitalii <pixel080808@gmail.com>
2025-04-17 20:19:43 +0300

    Оновлення

--- models/Brand.js
+++
@@ -1,8 +0,0 @@
-// models/Brand.js
-const mongoose = require('mongoose');
-
-const brandSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Brand', brandSchema);
--- models/Material.js
+++
@@ -1,8 +0,0 @@
-// models/Material.js
-const mongoose = require('mongoose');
-
-const materialSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Material', materialSchema);
