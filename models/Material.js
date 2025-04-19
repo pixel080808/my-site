0c98ec5d87f260fc9baf3a331a15d749bc580ca4
Vitalii <pixel080808@gmail.com>
2025-04-19 17:23:43 +0300

    Оновлення

--- models/Material.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const materialSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Material', materialSchema);
